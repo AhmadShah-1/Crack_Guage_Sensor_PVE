f26cdec87cdd835c1177c0fb53161ab87727efee
Ahmad Shah <sshah@pvedi-ae.com>
2025-11-18 12:10:36 -0500

    3

--- Testing/Version3/Read_Joystick/read_analog/read_analog.ino
+++ Testing/Version3/Read_Joystick/read_analog/read_analog.ino
@@ -6,8 +6,16 @@
  * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-joystick
  */
 
+ /*
+ For esp32 Wroom
  #define VRX_PIN  39 // ESP32 pin GPIO39 (ADC3) connected to VRX pin
  #define VRY_PIN  36 // ESP32 pin GPIO36 (ADC0) connected to VRY pin
+ */
+
+ // Pin definitions for ESP32-CAM (avoiding camera pins)
+ #define VRX_PIN  33 // ESP32-CAM GPIO33 (ADC1_CH5) connected to VRX pin
+ #define VRY_PIN  32 // ESP32-CAM GPIO32 (ADC1_CH4) connected to VRY pin
+ // Note: GPIO32 is PWDN on ESP32-CAM. If camera needs power control, use GPIO2 or GPIO12 instead
  
  int valueX = 0; // to store the X-axis value
  int valueY = 0; // to store the Y-axis value
